295d8b40f889df0cfd744f6ed1e95bfecacbae16
Boaz Hazan <boazh2@gmail.com>
2018-11-08 00:32:53 +0200

    Inital commit

--- app/src/main/java/com/boazz300/movietrailer/MoviePageActivity.java
+++ app/src/main/java/com/boazz300/movietrailer/MoviePageActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MoviePageActivity extends AppCompatActivity {
 
@@ -14,10 +15,14 @@ public class MoviePageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_page);
     }
     public void openTrailer(View view){
-        String video_path = "https://www.youtube.com/watch?v=6ZfuNTqbHE8";
-        Uri uri = Uri.parse(video_path);
-        uri = Uri.parse("vnd.youtube:"  + uri.getQueryParameter("v"));
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+        Button btnTrailer = findViewById(R.id.details_btn_trailer);
+        btnTrailer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String trailerUrl = MoviePageActivity.this.getString(R.string.infinity_war_trailer);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(trailerUrl));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
