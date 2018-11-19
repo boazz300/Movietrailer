dcca27b2441b001a0c76e2cea593f3e4df49ae50
Boaz Hazan <boazh2@gmail.com>
2018-11-20 00:29:05 +0200

    Inital commit

---
+++ app/src/main/java/com/boazz300/movietrailer/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.boazz300.movietrailer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        /****** Create Thread that will sleep for 5 seconds****/
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),MoviePageActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
